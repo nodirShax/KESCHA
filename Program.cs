992296dffacce2283a09cd3c2aae2481c49dc128
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-01 03:25:17 -0800

    INFRA:Upload-Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter your age: ");
+string ageString = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageString);
+System.Console.WriteLine($"Succesfully converted {ageString}");
+int keschaAge = 3;
+int ageDifference = age - keschaAge;
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
