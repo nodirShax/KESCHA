e2ec4ad43fac8323af128d5c99a09a8fc83797ca
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-01 13:01:15 -0800

    INFRA: oop-initial

---
+++ Classes/Animal.cs
@@ -0,0 +1,64 @@
+namespace KESCHA.Classes
+{
+    class Animal
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int AgeDifference { get; set; }
+        public Animal(int age,string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public void CalculateAndPrintAgeDifferenceWithoutReturn(int userAge)
+        {
+            AgeDifference = userAge - Age;
+            System.Console.WriteLine(AgeDifference);
+        }
+        public void CompareAge(int userAge)
+        {
+            if(Age > userAge)
+            {
+                System.Console.WriteLine("You are older!");
+            }
+            else if(Age == userAge)
+            {
+                System.Console.WriteLine("You are equal!");
+            }
+            else
+            {
+                System.Console.WriteLine("You are younger!");
+            }
+        }
+        public void Greet(string userName)
+        {
+            if(userName == "")
+            {
+                System.Console.WriteLine("Hello Unknown");
+            }
+            else
+            {
+                System.Console.WriteLine($"Hello {userName}");
+            }
+        }
+
+        public void TelAboutFriends(int userAge,string userName)
+        {
+            string[] friends = new string[3];
+            friends[0] = "Kasee";
+            friends[1] = "Vase";
+            friends[2] = userName;
+
+            int[] friendsAges = new int[3];
+            friendsAges[0] = 4;
+            friendsAges[1] = 2;
+            friendsAges[2] = userAge;
+
+            for(int i = 0; i < 3; i++)
+            {
+                System.Console.WriteLine($"{friends[i]} is {friendsAges[i]} years old");
+            }
+        }
+    }
+}
