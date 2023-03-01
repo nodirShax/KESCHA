bcb1c006a14035bb3760c35ab7175abaaf5aa8de
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-01 13:02:05 -0800

    INFRA: oop-initial

--- Program.cs
+++ Program.cs
@@ -1,40 +1,20 @@
 ﻿using System;
+using KESCHA.Classes;
+
+Animal kescha = new Animal(name : "Kescha", age : 3);
 System.Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-string greeting = $"Hello {name}";
-System.Console.WriteLine(greeting);
+string userName = Console.ReadLine();
+kescha.Greet(userName);
 
-System.Console.Write("Enter your age: ");
-string ageString = Console.ReadLine();
+System.Console.Write("Enter yor age : ");
+string ageAsString = Console.ReadLine();
+
 System.Console.WriteLine("Converting...");
-int age = Convert.ToInt32(ageString);
-System.Console.WriteLine($"Succesfully converted {ageString}");
-int keschaAge = 3;
-int ageDifference = age - keschaAge;
-if(age > keschaAge)
-{
-    System.Console.WriteLine("You are older!");
-}
-else if(age == keschaAge)
-{
-    System.Console.WriteLine("You are equal!");
-}
-else
-{
-    System.Console.WriteLine("You are younger!");
-}
-System.Console.WriteLine("Let me tell you about my friends?");
-string[] friends = new string[3];
-friends[0] = "Kasee";
-friends[1] = "Vase";
-friends[2] = name;
+int userAge = Convert.ToInt32(ageAsString);
+System.Console.WriteLine("Convert Succesfully!");
 
-int[] friendsAges = new int[3];
-friendsAges[0] = 4;
-friendsAges[1] = 2;
-friendsAges[2] = age;
+kescha.CalculateAndPrintAgeDifferenceWithoutReturn(userAge);
+kescha.CompareAge(userAge);
+kescha.TelAboutFriends(userAge,userName);
 
-for(int i = 0; i < 3; i++)
-{
-    System.Console.WriteLine($"{friends[i]} is {friendsAges[i]} years old");
-}
+
